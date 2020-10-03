fbcb6414f642ee92ae86f23df8fe21b2979b22ea
Nitesh <nitesh26589@gmail.com>
2020-10-04 01:36:57 +0530

    Algorithm made more Efficient just added some more conditions and base cases

--- src/App.js
+++ src/App.js
@@ -1,113 +1,273 @@
 import React, { Component } from "react";
+import logo from "./path.png";
 import Grid from "./grid";
-import end from "./finish.png";
-import start from "./download.png";
+import PriorityQueue from "./priorityq";
+import end from "./times-circle-solid.svg";
+import start from "./play-solid.svg";
 
 const Modal = () => {
   return (
     <div
-      style={{ position: "top", top: "200px", color: "white", left: "44vw" }}
-      className="p-4 bg-dark rounded animation-target"
+      style={{ position: "fixed", top: "200px", color: "white", left: "44vw" }}
+      className="p-4 bg-dark rounded shadow-lg animation-target"
     >
-      Sorry You Can't reach end point!
+      Path to the target Not Found !
     </div>
   );
 };
 export class App extends Component {
   state = {
-    height: 15,
-    width: 30,
-    start: [7, 5],
-    end: [7, 25],
+    height: 20,
+    width: 40,
+    start: [10, 5],
+    end: [10, 34],
+
     path: [],
+    heuristic: null,
     current: null,
-     grid: Array(20)
-       .fill()
-    .map(() => Array(40).fill(0)),
+    grid: Array(20)
+      .fill()
+      .map(() => Array(40).fill(0)),
     visualize: false,
     method: 0,
     showModal: false,
     speed: 1,
   };
-  
+  componentDidMount() {
+    //Calculating Heriustic for A* Search
+    let heuristic = Array(20)
+      .fill()
+      .map(() => Array(40).fill(0));
 
+    for (let i = 0; i < this.state.height; i++) {
+      for (let j = 0; j < this.state.width; j++) {
+        heuristic[i][j] =
+          Math.abs(this.state.end[0] - i) + Math.abs(this.state.end[1] - j);
+      }
+    }
+    this.setState({ heuristic });
+  }
+
+  visualizeA = async () => {
+    this.clearPath();
+    this.setState({ visualize: true });
+    let flag = 0;
+    let path = Array(20)
+      .fill()
+      .map(() => Array(40).fill([]));
+    let q = new PriorityQueue(); //priority queue for A* Search
+
+    q.enqueue(
+      this.state.start,
+      this.state.heuristic[this.state.start[0]][this.state.start[1]]
+    );
+
+    while (!q.isEmpty()) {
+      await new Promise((done) => setTimeout(() => done(), this.state.speed)); //To slow down the animation
+
+      let grid = this.state.grid;
+      let current = q.front().element;
+
+      q.dequeue();
+      this.setState({ current });
+      if (
+        current[0] === this.state.end[0] &&
+        current[1] === this.state.end[1]
+      ) {
+        flag = 1;
+        console.log(current);
+        break;
+      }
+      let list = [];
+
+      if (
+        current[1] !== this.state.width - 1 &&
+        (
+          grid[current[0]][current[1] + 1] === 0)
+      ) {
+        list.push([current[0], current[1] + 1]);
+        if (
+          path[current[0]][current[1] + 1].length === 0 ||
+          path[current[0]][current[1] + 1].length >
+            [...path[current[0]][current[1]], current].length
+        ) {
+          q.enqueue(
+            [current[0], current[1] + 1],
+            this.state.heuristic[current[0]][current[1] + 1]
+          );
+          path[current[0]][current[1] + 1] = [
+            ...path[current[0]][current[1]],
+            current,
+          ];
+        }
+      }
+      if (
+        current[0] !== this.state.height - 1 &&
+        (
+          grid[current[0] + 1][current[1]] === 0)
+      ) {
+        list.push([current[0] + 1, current[1]]);
+        if (
+          path[current[0] + 1][current[1]].length === 0 ||
+          path[current[0] + 1][current[1]].length >
+            [...path[current[0]][current[1]], current]
+        ) {
+          q.enqueue(
+            [current[0] + 1, current[1]],
+            this.state.heuristic[current[0] + 1][current[1]]
+          );
+          path[current[0] + 1][current[1]] = [
+            ...path[current[0]][current[1]],
+            current,
+          ];
+        }
+      }
+      if (
+        current[0] !== 0 &&
+        (
+          grid[current[0] - 1][current[1]] === 0)
+      ) {
+        list.push([current[0] - 1, current[1]]);
+        if (
+          path[current[0] - 1][current[1]].length === 0 ||
+          path[current[0] - 1][current[1]].length >
+            [...path[current[0]][current[1]], current]
+        ) {
+          q.enqueue(
+            [current[0] - 1, current[1]],
+            this.state.heuristic[current[0] - 1][current[1]]
+          );
+          path[current[0] - 1][current[1]] = [
+            ...path[current[0]][current[1]],
+            current,
+          ];
+        }
+      }
+      if (
+        current[1] !== 0 &&
+        (
+          grid[current[0]][current[1] - 1] === 0)
+      ) {
+        list.push([current[0], current[1] - 1]);
+        if (
+          path[current[0]][current[1] - 1].length === 0 ||
+          path[current[0]][current[1] - 1].length >
+            [...path[current[0]][current[1]], current].length
+        ) {
+          q.enqueue(
+            [current[0], current[1] - 1],
+            this.state.heuristic[current[0]][current[1] - 1]
+          );
+          path[current[0]][current[1] - 1] = [
+            ...path[current[0]][current[1]],
+            current,
+          ];
+        }
+      }
+
+      grid[current[0]][current[1]] = 3;
+
+      this.setState({ grid });
+    }
+    //Modal to show when No Path is Found
+    if (flag === 0) {
+
+      this.setState({ current: null, showModal: true });
+      setTimeout(() => this.setState({ showModal: false }), 5000);
+
+    } else {
+     
+      //To show the Path
+      for (
+        let i = 0;
+        i < path[this.state.end[0]][this.state.end[1]].length;
+        i++
+      ) {
+        await new Promise((done) => setTimeout(() => done(), 1));//To slow down the Path showing animation
+        this.setState({
+          path: path[this.state.end[0]][this.state.end[1]].slice(0, i + 1),
+        });
+      }
+    }
+
+    this.setState({ visualize: false });
+  };
+  
   visualizeDijkstra = async () => {
-    //BFS being implemented as Dijkstra's Algorithm, becomes BFS in Unweighted Graphs
-   let path = Array(20).fill().map(() => Array(40).fill([]));
-   
+    //Note: Since This Web App only supports unweighted graph , BFS is being implemented as Dijkstra's Algorithm becomes BFS in Unweighted Graphs
+    let path = Array(20)
+      .fill()
+      .map(() => Array(40).fill([]));
     let q = [this.state.start];//queue to implement BFS
     let flag = 1;
     this.clearPath();
+
     this.setState({ visualize: true });//To disable buttons
 
+    let grid = this.state.grid;
 
     while (q.length !== 0 && flag) {
-      //To slow down the speed of Animation
-      // a promise is returned becoz in future the speed may change
-      await new Promise((done) => setTimeout(() => done(), this.state.speed));
-      let current = q[0];
-      q.shift();
-      this.setState({ current });
+     
 
-      let grid = this.state.grid;
+      const current = q[0];
+
+      q.shift();
+      if(grid[current[0]][current[1]]!==0){
+        continue;
+      }
       
-      if (current[0] === this.state.end[0] &&current[1] === this.state.end[1]) {
-        console.log(current);
-        console.log(path);
-        flag = 0;//when we reach end point set the flag to 0 and while loop terminates
+      if (
+        current[0] === this.state.end[0] &&
+        current[1] === this.state.end[1]
+      ) {
+        
+        flag = 0;
       } else {
         let list = [];
-        //now only 4 cases can arrive
 
-        /*1st case:-if current(x) of 0 is not in last row and
-        the next neighbour down is unvisited
-        */
-        if (current[0] !== this.state.height - 1 && grid[current[0] + 1][current[1]] === 0) {
-
-          //down neigh is pushed into list
+        if (
+          current[0] !== this.state.height - 1 &&
+          grid[current[0] + 1][current[1]] === 0
+        ) {
           list.push([current[0] + 1, current[1]]);
-          //console.log(current);
-          path[current[0] + 1][current[1]] = [...path[current[0]][current[1]],current];
-          //console.log(path[current[0] + 1][current[1]]);
+          path[current[0] + 1][current[1]] = [
+            ...path[current[0]][current[1]],
+            current,
+          ];
+
           path[current[0] + 1][current[1]].push(current);
         }
-
-        /*2nd case:-if current(y) of 1 is not the last column and
-        the next neighour right is unvisited */
-
-        if (current[1] !== this.state.width - 1 &&grid[current[0]][current[1] + 1] === 0) {
-          //right neigh is pushed into list
+        if (
+          current[1] !== this.state.width - 1 &&
+          grid[current[0]][current[1] + 1] === 0
+        ) {
           list.push([current[0], current[1] + 1]);
-          
-          path[current[0]][current[1] + 1] = [...path[current[0]][current[1]],current,];
-          //console.log( path[current[0]][current[1] + 1]);
+          path[current[0]][current[1] + 1] = [
+            ...path[current[0]][current[1]],
+            current,
+          ];
         }
-
-        /*3rd case:-if current(x) is not zero and up is not visited */
         if (current[0] !== 0 && grid[current[0] - 1][current[1]] === 0) {
-          //up neigh is pushed into list
           list.push([current[0] - 1, current[1]]);
-          
-          path[current[0] - 1][current[1]] = [...path[current[0]][current[1]],current,];
-          //console.log( path[current[0] - 1][current[1]])
+          path[current[0] - 1][current[1]] = [
+            ...path[current[0]][current[1]],
+            current,
+          ];
         }
-
-        /*4th case:= if current(y) is not zero and left is unvisited */
         if (current[1] !== 0 && grid[current[0]][current[1] - 1] === 0) {
-          //left neigh is pushed into list
           list.push([current[0], current[1] - 1]);
-          
-          path[current[0]][current[1] - 1] = [...path[current[0]][current[1]],current,];
-          //console.log(path[current[0]][current[1] - 1]);
-          
+          path[current[0]][current[1] - 1] = [
+            ...path[current[0]][current[1]],
+            current,
+          ];
         }
 
-        //mark current node as visited
+     
         grid[current[0]][current[1]] = 3;
 
-        //changing the state of grid
-        this.setState({ grid });
-        //appending the list of neighbours to the queue
+        this.setState({ grid ,current});
+        await new Promise((done) => setTimeout(() => done(), this.state.speed));//To slow down the speed of Animation
+
         q = q.concat(list);
       }
     }
@@ -117,13 +277,16 @@ export class App extends Component {
       setTimeout(() => this.setState({ showModal: false }), 5000);
     } else {
      //To show the Path between start and the Target
-      for (let i = 0;i < path[this.state.end[0]][this.state.end[1]].length;i++) {
-        await new Promise((done) => setTimeout(() => done(), 0.1));//To slow down the animation
-        this.setState(
-          { path: path[this.state.end[0]][this.state.end[1]].slice(0, i + 1),}
-        );
+      for (
+        let i = 0;
+        i < path[this.state.end[0]][this.state.end[1]].length;
+        i++
+      ) {
+        await new Promise((done) => setTimeout(() => done(), 1));//To slow down the animation
+        this.setState({
+          path: path[this.state.end[0]][this.state.end[1]].slice(0, i + 1),
+        });
       }
-      
     }
 
     this.setState({ visualize: false });//To enable buttons again
@@ -131,15 +294,12 @@ export class App extends Component {
 
   clearPath = () => {
     //To clear the path between start and target
-    //clearing visited nodes
     this.setState((state) => {
       return {
         grid: state.grid.map((row) =>
           row.map((obj) => {
-            if (obj === 1 || obj === 0) 
-            return obj;
-            else 
-            return 0;
+            if (obj === 1 || obj === 0) return obj;
+            else return 0;
           })
         ),
         path: [],
@@ -148,25 +308,22 @@ export class App extends Component {
       };
     });
   };
-
-
-  toggleWall = (x, y, type) => {
+  toggleWall = (y, x, type) => {
     //Convert a unvisited node to a wall and vice versa
     if (type === 1) {
       this.setState((state) => {
         let grid = state.grid;
-        grid[x][y] = 0;
+        grid[y][x] = 0;
         return { grid };
       });
     } else {
       this.setState((state) => {
         let grid = state.grid;
-        grid[x][y] = 1;
+        grid[y][x] = 1;
         return { grid };
       });
     }
   };
-
   render() {
     return (
       <div>
@@ -174,33 +331,74 @@ export class App extends Component {
         {this.state.showModal ? <Modal /> : null}
 
         <div
-
           className="navbar navbar-dark "
-          style={{ backgroundColor: "#f3c623"}}
+          style={{ backgroundColor: "#424874", color: "white" }}
         >
           <div className="navbar-brand">
-            PathFinder
+            <img
+              src={logo}
+              style={{ width: "auto", height: "30px" }}
+              alt="logo"
+              className="mr-4"
+            />
+            Shortest Path Visualizer
           </div>
-            <button
-              className=" btn btn-primary ml-auto mr-2"
-              onClick={() =>
-                this.visualizeDijkstra()
-              }
-             disabled={this.state.visualize}
-            >
-              Visualize Dijkstra's Algorithm
-            </button>
 
-            <div className="dropdown">
+          <div className="row">
+            <div className="dropdown mx-4">
               <button
-                className="btn btn-primary dropdown-toggle mx-2"
+                className="btn  dropdown-toggle"
                 type="button"
                 id="dropdownMenuButton"
-               
+                style={{ backgroundColor: "#dcd6f7" }}
                 data-toggle="dropdown"
                 aria-haspopup="true"
                 aria-expanded="false"
-                disabled={this.state.visualize}//for disabling the button
+                disabled={this.state.visualize}
+              >
+                Algorithm
+              </button>
+              <div
+                className="dropdown-menu"
+                aria-labelledby="dropdownMenuButton"
+              >
+                <button
+                  className="dropdown-item"
+                  onClick={() => this.setState({ method: 0 })}
+                >
+                  Dijkstra's Algorithm
+                </button>
+                <button
+                  className="dropdown-item"
+                  onClick={() => this.setState({ method: 1 })}
+                >
+                  A* Search
+                </button>
+              </div>
+            </div>
+            <button
+              className=" btn mx-4"
+              onClick={() =>
+                this.state.method === 0
+                  ? this.visualizeDijkstra()
+                  : this.visualizeA()
+              }
+              style={{ backgroundColor: "#dcd6f7" }}
+              disabled={this.state.visualize}
+            >
+              Visualize{" "}
+              {this.state.method === 0 ? " Dijkstra's Algorithm " : " A* "}
+            </button>
+            <div className="dropdown mx-4">
+              <button
+                className="btn dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton"
+                style={{ backgroundColor: "#dcd6f7" }}
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+                disabled={this.state.visualize}
               >
                 {this.state.speed === 1
                   ? "Fast"
@@ -232,39 +430,36 @@ export class App extends Component {
                 </button>
               </div>
             </div>
-
-
             <button
-              className=" btn-primary btn mx-2"
+              className=" btn mx-4"
               onClick={() =>
                 this.setState({
                   grid: Array(20)
                     .fill()
-                    .map(() => Array(40).fill(0)),//grid is filled with zeros
-                  path: [],//the path array is emptied
-                  current: null,//current array is made null
+                    .map(() => Array(40).fill(0)),
+                  path: [],
+                  current: null,
                 })
               }
-              disabled={this.state.visualize}//the state of button is disabled when program is runnin
+              style={{ backgroundColor: "#dcd6f7" }}
+              disabled={this.state.visualize}
             >
               Clear Walls
             </button>
-
             <button
-              className="btn-primary btn mx-2"
+              className=" btn mx-4"
               onClick={() => this.clearPath()}
-              
+              style={{ backgroundColor: "#dcd6f7" }}
               disabled={this.state.visualize}
             >
               Clear Path
             </button>
           </div>
-        
-
+        </div>
         <div className="container-fluid">
           <div className="row pt-4">
             <div className="row mx-4">
-              Start Point :-
+              start node :
               <div
                 className="mx-2"
                 style={{
@@ -281,7 +476,7 @@ export class App extends Component {
               </div>
             </div>
             <div className="row mx-4">
-              End Point:-
+              End Node:
               <div
                 className="mx-2"
                 style={{
@@ -298,32 +493,32 @@ export class App extends Component {
               </div>
             </div>
             <div className="row mx-4">
-              Visited Node:-
+              Visited Node:
               <div
                 className="mx-2"
                 style={{
                   width: "30px",
                   height: "30px",
-                  backgroundColor: "#a6b1e1",
+                  backgroundColor: "#8ccbbe",
                   WebkitUserSelect: "none",
                 }}
               ></div>
             </div>
             <div className="row mx-4">
-              Unvisited Node:-
+              Unvisited Node:
               <div
                 className="mx-2"
                 style={{
                   width: "30px",
                   height: "30px",
                   backgroundColor: "white",
-                  border: "4px solid lightblue",
+                  border: "1px solid lightblue",
                   WebkitUserSelect: "none",
                 }}
               ></div>
             </div>
             <div className="row mx-4">
-              Shortest Path Node:-
+              Shortest Path Node:
               <div
                 className="mx-2"
                 style={{
@@ -335,27 +530,14 @@ export class App extends Component {
               ></div>
             </div>
             <div className="row mx-4">
-              Wall Node:-
+              Wall Node:
               <div
                 className="mx-2"
                 style={{
                   width: "30px",
                   height: "30px",
-                  backgroundColor: "#FF0000",
+                  backgroundColor: "#202060",
                   WebkitUserSelect: "none",
-                }}
-              ></div>
-            </div>
-            <div className="row mx-4">
-              Current Node:-
-              <div
-                className="mx-2"
-                style={{
-                  width: "30px",
-                  height: "30px",
-                  backgroundColor: "pink",
-                  WebkitUserSelect: "none",
-                  borderRadius: "50%",
                 }}
               ></div>
             </div>
